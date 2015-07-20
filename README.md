a9c33034871c69f46273dcd2fa8990bde2c31b4f
Sebastien Saunier <seb@saunier.me>
2015-07-20 21:07:34 +0200

    Mixpanel avatars for Kitt

---
+++ README.md
@@ -0,0 +1,3 @@
+My collection of custom executed JS.
+
+Works thanks to [defunkt/dotjs](https://github.com/defunkt/dotjs) and [dotjs Firefox Extension](https://addons.mozilla.org/en-US/firefox/addon/dotjs/)
---
+++ mixpanel.com.js
@@ -0,0 +1,39 @@
+(function() {
+  var PROJECTS = [
+    {
+      id: '702849',
+      pattern: function(distinctId) {
+        return "http://kitt.lewagon.org/users/" + distinctId + "/avatar";
+      }
+    }
+  ];
+
+  function parseDistinctId(anchor) {
+    return anchor.split("=")[1];
+  }
+
+  function replaceImage($element, project, id) {
+    if (id) {
+      $element.attr('src', project.pattern.call(this, id));
+    }
+  }
+
+  PROJECTS.forEach(function(project) {
+    if (window.location.pathname.indexOf(project.id) != -1) {
+      $(document).ready(function() {
+        // People list
+        $('.avatar_cell').each(function(i, item) {
+          var id = parseDistinctId($(item).find('a').attr('href'));
+          replaceImage($(item).find('img'), project, id);
+        });
+
+        // User profile
+        if ($('.avatar').length) {
+          var id = parseDistinctId(window.location.hash);
+          replaceImage($('.avatar img'), project, id);
+        }
+      });
+    }
+  });
+})();
+
