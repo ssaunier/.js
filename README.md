5521eda5ceac26ff3e16cde0f19bd34fcae7412b
Sebastien Saunier <seb@saunier.me>
2015-07-25 11:55:45 +0200

    Parsing distinctId can't be generalized

--- README.md
+++ README.md
@@ -4,27 +4,4 @@ Works thanks to [defunkt/dotjs](https://github.com/defunkt/dotjs) and [dotjs Fir
 
 ## Mixpanel Avatar Replacer
 
-In the [mixpanel.com.js](mixpanel.com.js) file, you'll find the code to replace Mixpanel default gravatar with your **own avatars** from **your** application. Just declare your Mixpanel projects in the file like this:
-
-```js
-var PROJECTS = [
-  {
-    id: '123456',  // Your mixpanel project id, look in the URL!
-    pattern: function(distinctId) {
-      // `distinctId` is the Mixpanel distinct id of the user.
-      // Usually it's the User id from your database (if you used
-      // `alias` or `identify` that way)
-      return "http://yourapp.com/users/" + distinctId + "/avatar";
-    }
-  },
-  {
-    id: '1826472',
-    pattern: function(distinctId) {
-      return "http://anotherapp.com/" + distinctId + "/avatar";
-    }
-  }
-];
-// You can have more element in the `PROJECTS` array by the way, or just one.
-```
-
-Your app (at `yourapp.com`) should respond to the `/users/:id/avatar` route (again that's the exemple) with an image or a redirection to an image.
+In the [mixpanel.com.js](mixpanel.com.js) file, you'll find the code to replace Mixpanel default gravatar with your **own avatars** from **your** application.
--- mixpanel.com.js
+++ mixpanel.com.js
@@ -4,19 +4,30 @@
       id: '702849',
       pattern: function(distinctId) {
         return "http://kitt.lewagon.org/users/" + distinctId + "/avatar";
+      },
+      parseDistinctId: function(item) {
+        if (typeof item === "undefined") {
+          return window.location.hash.split("=")[1];
+        } else {
+          var anchor = $(item).find('a').attr('href')
+          return anchor.split("=")[1];
+        }
       }
     },
     {
       id: '633295',
       pattern: function(distinctId) {
         return "https://ondemand.lewagon.org/users/" + distinctId + "/avatar";
+      },
+      parseDistinctId: function(item) {
+        if (typeof item === "undefined") {
+          return $('[title=id]').parents('.mp_editable_property').find('.value_contents').text();
+        } else {
+          return $(item).parent().find('.prop_value_cell.1 div').text();
+        }
       }
     }
   ];
-
-  function parseDistinctId(anchor) {
-    return anchor.split("=")[1];
-  }
 
   function replaceImage($element, project, id) {
     if (id) {
@@ -29,13 +40,13 @@
       $(document).ready(function() {
         // People list
         $('.avatar_cell').each(function(i, item) {
-          var id = parseDistinctId($(item).find('a').attr('href'));
+          var id = project.parseDistinctId(item);
           replaceImage($(item).find('img'), project, id);
         });
 
         // User profile
         if ($('.avatar').length) {
-          var id = parseDistinctId(window.location.hash);
+          var id = project.parseDistinctId();
           replaceImage($('.avatar img'), project, id);
         }
       });
